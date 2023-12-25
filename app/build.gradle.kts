33b27671ab5fa6c3c6b2c1f576c7d7ae74eb8759
Dinesh N <dinesh.prabha1205@gmail.com>
2023-12-25 23:46:12 +0530

    added home fragment and bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("com.github.bumptech.glide:glide:4.13.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+//    implementation("androidx.arch.lifecycle:extensions:1.1.1")
 }
---
+++ app/src/main/java/com/dineshprabha/chefsaga/ui/activity/FoodActivity.kt
@@ -0,0 +1,12 @@
+package com.dineshprabha.chefsaga.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dineshprabha.chefsaga.R
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+    }
+}
